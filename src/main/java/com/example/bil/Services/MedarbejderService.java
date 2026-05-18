c326b58a129f1cc71ca4ee5401cc282b46477007
LucasKeszler <lucas.keszler@gmail.com>
2026-05-18 17:40:24 +0200

    removed debugging from medarbejder login

--- src/main/java/com/example/bil/Services/MedarbejderService.java
+++ src/main/java/com/example/bil/Services/MedarbejderService.java
@@ -17,26 +17,12 @@ public class MedarbejderService {
 
     public Medarbejder login(String username, String password) {
         try {
-            System.out.println("LOGIN input username = [" + username + "]");
-            System.out.println("LOGIN input password = [" + password + "]");
+            Medarbejder medarbejder = medarbejderRepository.getMedarbejderByUsername(username.trim());
 
-            Medarbejder medarbejder = medarbejderRepository.getMedarbejderByUsername(username);
-
-            if (medarbejder == null) {
-                System.out.println("LOGIN: Ingen medarbejder fundet");
-                return null;
-            }
-
-            System.out.println("LOGIN DB username = [" + medarbejder.getUsername() + "]");
-            System.out.println("LOGIN DB password = [" + medarbejder.getPassword() + "]");
-            System.out.println("LOGIN DB rolle = [" + medarbejder.getRolle() + "]");
-
-            if (medarbejder.getPassword().equals(password)) {
-                System.out.println("LOGIN: Success");
+            if (medarbejder != null &&
+                    medarbejder.getPassword().trim().equals(password.trim())) {
                 return medarbejder;
             }
-
-            System.out.println("LOGIN: Password matcher ikke");
 
         } catch (SQLException e) {
             e.printStackTrace();
