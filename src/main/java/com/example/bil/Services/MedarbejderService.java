2bc8ecd399a9ff90a840c9f107f1a0146ec77a4b
LucasKeszler <lucas.keszler@gmail.com>
2026-05-18 17:14:18 +0200

    added error finding method to medarbejder login function

--- src/main/java/com/example/bil/Services/MedarbejderService.java
+++ src/main/java/com/example/bil/Services/MedarbejderService.java
@@ -15,17 +15,31 @@ public class MedarbejderService {
         this.medarbejderRepository = medarbejderRepository;
     }
 
-    public Medarbejder login (String username, String password) {
+    public Medarbejder login(String username, String password) {
         try {
+            System.out.println("LOGIN input username = [" + username + "]");
+            System.out.println("LOGIN input password = [" + password + "]");
+
             Medarbejder medarbejder = medarbejderRepository.getMedarbejderByUsername(username);
 
-            if (medarbejder !=null && medarbejder.getPassword().equals(password)) {
+            if (medarbejder == null) {
+                System.out.println("LOGIN: Ingen medarbejder fundet");
+                return null;
+            }
+
+            System.out.println("LOGIN DB username = [" + medarbejder.getUsername() + "]");
+            System.out.println("LOGIN DB password = [" + medarbejder.getPassword() + "]");
+            System.out.println("LOGIN DB rolle = [" + medarbejder.getRolle() + "]");
+
+            if (medarbejder.getPassword().equals(password)) {
+                System.out.println("LOGIN: Success");
                 return medarbejder;
             }
 
+            System.out.println("LOGIN: Password matcher ikke");
+
         } catch (SQLException e) {
             e.printStackTrace();
-
         }
 
         return null;
